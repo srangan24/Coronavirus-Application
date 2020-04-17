f0b74ff3cef5441102759b6c5e325175c44f5ab2
Srinath R <40651059+srangan24@users.noreply.github.com>
2020-04-17 00:18:29 -0400

    Intro page updates

--- corona_ui/lib/main.dart
+++ corona_ui/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   return runApp(MyApp());
@@ -8,62 +9,85 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.white,
-            body: Center(
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [Color(0xFF3F0052), Color(0xFF0076BA)]
-                        )
-                      ),
-                    ),
-                  Container(
-                    child: Image(
-                      image: AssetImage('Images/logo.png'),
-                    ),
-                    margin: EdgeInsets.only(top: 100.0),
-                  ),
-                  Card(
-                    color: Colors.white,
-                    margin: EdgeInsets.fromLTRB(40, 500, 40, 0),
-                    child: Center(
-                    child: Stack(
-                      children: <Widget>[
-                          Container(
-                            /* height: MediaQuery.of(context).size.height/3,
-                    width: MediaQuery.of(context).size.width/2,
-                    decoration: BoxDecoration(
-                      color: Color(0xFF3BAC88),
-                      borderRadius: new BorderRadius.only(
-                        bottomLeft: const Radius.circular(200.0),
-                  ) */
-                            height: 150.0,
-                            width: 300.0,
-                            decoration: BoxDecoration(
-                              color: Colors.blue,
-                              borderRadius: BorderRadius.all(Radius.circular(50.0))),
-                            child: Center(
-                              child: Text("Login",
-                              style: TextStyle(color: Colors.white, fontSize: 22),
-                              textAlign: TextAlign.center,),
-                            ),
-                          )
-                        ],
-                     )
-                    )
-                  ),
-                ],
-              )
-            )
-          )
-      );
-    }
+        home: IntroPage(),
+    );
   }
+}
+
+class LoginCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double cardHeight = MediaQuery.of(context).size.height*2.8/4;
+    double cardWidth = MediaQuery.of(context).size.width/10;
+    return Card(
+      shape: ContinuousRectangleBorder(
+        borderRadius: BorderRadius.only(topRight: Radius.circular(100), topLeft: Radius.circular(100)),
+      ),
+      color: Colors.white,
+      margin: EdgeInsets.fromLTRB(cardWidth, cardHeight, cardWidth, 0),
+      child: Center(
+      child: Column(
+        children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 25),
+              width: 200,
+              child: RaisedButton(
+                padding: EdgeInsets.all(5),
+                  child: Text("Log in",
+                    style: GoogleFonts.nunito(
+                      textStyle: TextStyle(color: Colors.white, fontSize: 30),
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  color: Color(0xFF0076BA),
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  onPressed: () {},
+                ),
+            ),
+        ]
+      )
+      )
+    );
+  }
+}
+
+class IntroPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Color(0xFF3F0052), Color(0xFF0076BA)]
+                  )
+                ),
+              ),
+            Container(
+              child: Image(
+                image: AssetImage('Images/logo.png'),
+              ),
+              margin: EdgeInsets.only(top: 100.0),
+            ),
+            LoginCard(),
+          ],
+        )
+      )
+    );
+  }
+}
+        
+        
+        
+        
 
 
 /* child: FlatButton(
